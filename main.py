26916a50b016da82eb166726a1e13cc11f78f5c9
David Chou <31830900+schmintendo@users.noreply.github.com>
2019-02-26 22:25:35 -0600

    updated "config"
    config is now command line arguments I guess...

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import csv #for reading csvs
 import sys #for reading command line arguments
 
 #Cmd Line Args checking
-if not len(sys.argv) == 2:
-	print("Correct syntax is: \"py(thon3) main.py filename\"")
+if not len(sys.argv) == 5:
+	print("Correct syntax is: \"py(thon3) main.py filename processingRate(nanoseconds) runTime(nanoseconds) wantedBufferSize\"")
 	print("Terminating...")
 	sys.exit()
 
@@ -20,8 +20,13 @@ packetBuffer = [] #this is the list that packet numbers go in
 timeStamp = [] # idk if we need this but if we do we have it!
 day = "Undefined"
 
-#
+#Command Line Arg Instantiation
 filename = sys.argv[1] # first arg of command line must be filename of csv
+processingRate = int(sys.argv[2]) # second arg of command line must be processing rate in int (nanoseconds)
+runTime = int(sys.argv[3]) #  arg of command line must be runtime in int (nanoseconds)
+wantedBufferSize = int(sys.argv[4]) # fourth arg of command line must be desired buffer size (just a number)
+
+
 #main stuff here
 
 with open(filename, "r") as f:
